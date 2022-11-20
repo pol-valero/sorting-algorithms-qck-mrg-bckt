e4ce4a491d158ef8f5064a06efb4d8cf29d0e4dc
Pol Valero <pol.valero@students.salle.url.edu>
2022-11-20 09:40:53 +0100

    S'han afegit noves classes i funcions

---
+++ src/SortingAlgorithms.java
@@ -0,0 +1,22 @@
+package src;
+
+public class SortingAlgorithms {
+
+    public static void quickSortAge () {
+
+    }
+
+    public static void mergeSortName () {
+
+    }
+
+    public static void bucketSortCapabilities () {
+        //Per ordenar vaixells per prestacions, primer hem de ponderar els seus
+        //atributs (pes, eslora, capacitat, velocitat max) amb un pes diferent
+        //cadascun. Un cop això estigui fet, per cada vaixell tindrem un resultat
+        //de la suma ponderada de les seves prestacions. Aquest resultat es el que
+        //utilitzarem per a ordenar els vaixells
+
+    }
+
+}
