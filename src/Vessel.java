ad2d8377bf5d7e19ebc9802db4fe89ca74cf91d1
Pol Valero <pol.valero@students.salle.url.edu>
2022-11-20 09:53:34 +0100

    S'ha creat la classe "Vessel" que nomes servira per a guardar la informació d'un vaixell

--- src/Vessel.java
+++ src/Vessel.java
@@ -1,4 +1,32 @@
 package src;
 
 public class Vessel {
+
+
+    public int id;
+    public String name;
+    public String type;
+    public float weight;
+    public float length;
+    public int capacity;
+    public int n_competitions;
+    public String status;
+    public int speed;
+    public String center;
+
+    public Vessel (int id, String name, String type, float weight, float length, int capacity, int n_competitions, String status, int speed, String center) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.weight = weight;
+        this.length = length;
+        this.capacity = capacity;
+        this.n_competitions = n_competitions;
+        this.status = status;
+        this.speed = speed;
+        this.center = center;
+    }
+
+
+
 }
